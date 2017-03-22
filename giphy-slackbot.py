34964daeb330b6ecf997d88a096567efa7217529
Josh <jmak2015@gmail.com>
2017-03-21 17:09:07 -0700

    Add Configuration File Support

--- giphy-slackbot.py
+++ giphy-slackbot.py
@@ -1,39 +1,53 @@
-import os
+from slackclient import SlackClient
 import json
+import os.path
+import sys.exit
 import time
 import urllib2
-from slackclient import SlackClient
 
-slack_client = SlackClient('''[ INSERT SLACKBOT TOKEN HERE ]''')
+if os.path.isfile("config.json"):
+    with open("config.json") as config:
+        data = json.load(config)
+        if data["token"]:
+            slack_client = SlackClient(data["token"])
+        else:
+            sys.exit("Setup failed. Token not found in configuration file.")
+else:
+    sys.exit("Setup failed. Configuration file missing.")
 
-def handle_command(channel):
-    api = json.loads(urllib2.urlopen("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=spongebob").read()) #REPLACE WITH GIPHY API PRODUCTION KEY IF NEEDED
+def handle_command(channel, key, tag):
+    url = "http://api.giphy.com/v1/gifs/random?api_key="
+    url += "dc6zaTOxFJmzC" if not key else key
+    if tag: url += "&tag=" + tag
+    api = json.loads(urllib2.urlopen(url).read())
     slack_client.api_call("chat.postMessage", channel=channel, text=api['data']['url'], as_user=True)
 
-def is_trigger_in_text(text):
+def is_trigger_in_text(text, triggers):
     if 'http://giphy.com/gifs/' in text:
         return False
-    triggers = ('spongebob', 'squarepants', 'patrick', 'squidward') #INSERT MORE KEYWORDS HERE
     for trigger in triggers:
         if trigger in text.toLowerCase():
             return True
     return False
 
-def parse_slack_output(slack_rtm_output):
+def parse_slack_output(slack_rtm_output, triggers):
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
-            if output and 'text' in output and is_trigger_in_text(output['text']):
+            if output and 'text' in output and is_trigger_in_text(output['text'], triggers):
                 return output['channel']
     return None
 
 if __name__ == "__main__":
     if slack_client.rtm_connect():
-        print("SpongeBot connected and running! It's giphy meme time! ;)")
-        while True:
-            channel = parse_slack_output(slack_client.rtm_read())
-            if channel:
-                handle_command(channel)
+        print("giphy-slackbot connected and running! It's giphy meme time! ;)")
+        while os.path.isfile("config.json"):
+            with open("config.json") as config:
+                data = json.load(config)
+                channel = parse_slack_output(slack_client.rtm_read(), data["triggers"])
+                if channel:
+                    handle_command(channel, data["key"], data["tag"])
             time.sleep(1)
+        sys.exit("Error loading configuration file.")
     else:
-        print("Connection failed. Invalid Slack token?")
+        sys.exit("Connection failed. Invalid Slack token?")
